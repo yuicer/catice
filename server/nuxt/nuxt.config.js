5b201d7cae954b07cd59d74daf865558f5b79e80
yuicer <yuicer@yeah.net>
2017-08-07 18:11:05 +0800

    1

--- server/nuxt/nuxt.config.js
+++ server/nuxt/nuxt.config.js
@@ -1,39 +1,59 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: 'starter',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+	/*
+	 ** Headers of the page
+	 */
+	head: {
+		title: 'starter',
+		meta: [
+			{
+				charset: 'utf-8'
+			},
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1'
+			},
+			{
+				hid: 'description',
+				name: 'description',
+				content: 'Nuxt.js project'
+			}
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+		link: [
+			{
+				rel: 'icon',
+				type: 'image/x-icon',
+				href: '/favicon.ico'
+			}
     ]
-  },
-  /*
-  ** Global CSS
-  */
-  css: ['~/assets/css/main.css'],
-  /*
-  ** Add axios globally
-  */
-  build: {
-    vendor: ['axios'],
-    /*
-    ** Run ESLINT on save
-    */
-    extend (config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  }
+	},
+	/*
+	 ** Global CSS
+	 */
+	css: ['~/assets/css/main.css'],
+	/*
+	 ** Add axios globally
+	 */
+	build: {
+		vendor: ['axios'],
+		extend(config, {
+			isClient
+		}) {
+			if (isClient) {
+				config.entry.vendor.unshift('babel-polyfill');
+			}
+		}
+		/*
+		 ** Run ESLINT on save
+		 */
+		//    extend (config, ctx) {
+		//      if (ctx.isClient) {
+		//        config.module.rules.push({
+		//          enforce: 'pre',
+		//          test: /\.(js|vue)$/,
+		//          loader: 'eslint-loader',
+		//          exclude: /(node_modules)/
+		//        })
+		//      }
+		//    }
+	}
 }
--- server/nuxt/server/api/users.js
+++ server/nuxt/server/api/users.js
@@ -1,27 +1,50 @@
-import { Router } from 'express'
-
+import {
+	Router
+} from 'express'
+import users from '../models/users'
 const router = Router()
-
-// Mock Users
-const users = [
-  { name: 'Alexandre' },
-  { name: 'Pooya' },
-  { name: 'Sébastien' },
-]
 
 /* GET users listing. */
 router.get('/users', function (req, res, next) {
-  res.json(users)
+
+	let docu = users.find({
+		age: 14
+	}, function (err, docs) {
+		if (err)
+			res.sendStatus(err)
+		else
+			res.send(docs)
+	})
+
 })
 
 /* GET user by ID. */
 router.get('/users/:id', function (req, res, next) {
-  const id = parseInt(req.params.id)
-  if (id >= 0 && id < users.length) {
-    res.json(users[id])
-  } else {
-    res.sendStatus(404)
-  }
+	let docu = users.find({},
+		(err, docs) => {
+			var id = parseInt(req.params.id)
+			if (id >= 0 && id < docs.length) {
+				res.json(docs[id])
+			} else {
+				res.sendStatus(404)
+			}
+		}
+	)
 })
+
+/* GET users listing. */
+//router.get('/users', function (req, res, next) {
+//  res.json(users)
+//})
+
+/* GET user by ID. */
+//router.get('/users/:id', function (req, res, next) {
+//  const id = parseInt(req.params.id)
+//  if (id >= 0 && id < users.length) {
+//    res.json(users[id])
+//  } else {
+//    res.sendStatus(404)
+//  }
+//})
 
 export default router
