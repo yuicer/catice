54bab469214d2410de4c61a34ca2b500af9a10a2
yuicer <yuicer@yeah.net>
2017-09-14 18:05:15 +0800

    更改 look-controls 不为拖拽模式

--- client/vue/src/components/Game/Snake/GluttonousSnake.vue
+++ client/vue/src/components/Game/Snake/GluttonousSnake.vue
@@ -5,17 +5,24 @@
 <!--		<a-light type="ambient"  color="#445451"></a-light>-->
 <!--  		<a-light type="point"  intensity="1"  position="2 4 4"></a-light>      -->
       	<a-sky color="#ECECEC"></a-sky>
-		<a-camera>
-   			<a-cursor
-   				
-   			></a-cursor>
-  		</a-camera>
+      	<a-box
+      		id="snake"
+      		position="0,0,0"
+      		color="#000"	      		
+      	>
+			<a-camera id="camera" look-controls="enabled: true; no_drag:true" wasd-controls="enabled: false">
+				<a-cursor id="cursor"></a-cursor>
+			</a-camera>
+      	</a-box>
+      	
+      	
       	<a-box
       		id="box"
 			position="-1 0 -5" 
 			rotation="45 45 0" 
 			color="#4CC3D9"
 			float="to: -1 0.5 -5;"
+			visible="false"	
 			scale_click
       	></a-box>
       
@@ -27,6 +34,7 @@
 	require('aframe')
 	require('aframe-animation-component');
 	require('./components')
+	//no-click
 	export default {
 		data() {
 			return {}
@@ -37,8 +45,12 @@
 		},
 		methods: {
 			test() {
-				var cc = document.querySelector('#box').object3D;
-				console.log(cc)
+				var dom1 = document.querySelector('#camera');
+				var dom2 = document.querySelector('#cursor');
+				var dom3 = document.querySelector('#box');
+				console.log(dom1.components)
+				console.log(dom2.components)
+//				dom3.setAttribute('visible',false)
 
 			}
 		},
