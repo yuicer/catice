b1d4be97096667f67fdf616a1b08d1c759caf4fa
yuicer <yuicer@yeah.net>
2017-10-20 17:43:37 +0800

    升级了 physics 版本，解决检测碰撞事件

--- client/vue/src/components/Game/index.vue
+++ client/vue/src/components/Game/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<a-scene physics="debug: true">
+		<a-scene physic>
 			<!--		<a-light type="ambient"  color="#445451"></a-light>-->
 			<a-assets>
 				<img id="advertisement" src="~assets/logo.png">
@@ -8,20 +8,22 @@
 
 			<a-sky color="#ddd"></a-sky>
 
-			<a-box id="snake" static-body position="0 0 0" color="#000" look-controls wasd-controls="fly:true">
+			<!-- <a-box id="snake" static-body position="0 0 0" color="#000" look-controls wasd-controls="fly:true">
 				<a-camera id="camera" position="0 1 1" look-controls="enabled:false" wasd-controls="enabled:false" user-height="0">
 					<a-cursor id="cursor"></a-cursor>
 				</a-camera>
-			</a-box>
-			<!-- <a-box static-body id="box" position="0 0 -5" color="#4CC3D9" float="to: -1 1 -5;" visible="false" scale_click>
-																																		</a-box> -->
+			</a-box> -->
+
+			<!--<a-box static-body id="box" position="0 0 -5" color="#4CC3D9" float="to: -1 1 -5;" visible="false" scale_click> -->
+			<a-camera id="camera" wasd-controls="fly:true">
+				<a-box id="box1" static-body position="0 -1 -1" width="1" height="1" depth="1"></a-box>
+			</a-camera>
+
+			</a-box> -->
 			<a-sphere position="5 0.5 -5" radius="2"></a-sphere>
-			<a-sphere dynamic-body position="3 0.5 -5" radius="1"></a-sphere>
-			<a-sphere dynamic-body position="1 0.5 -5" radius="1"></a-sphere>
-			<a-sphere dynamic-body position="-1 0.5 -5" radius="1"></a-sphere>
-			<a-entity id="box1" dynamic-body="shape: box; mass: 2" position="5 0.5 -5" width="1" height="1" depth="1"></a-entity>
-			<a-box id="box2" dynamic-body position="5 1.5 -5" width="1" height="1" depth="1"></a-box>
-			<a-box id="box3" dynamic-body position="5 2.5 -5" width="1" height="1" depth="1"></a-box>
+			<a-sphere id="sphere1" dynamic-body position="-1 0.5 -5" radius="1"></a-sphere>
+			<a-box id="box1" dynamic-body position="5 1 -5" width="1" height="1" depth="1"></a-box>
+			<a-box id="box2" dynamic-body position="5 0 -5" width="1" height="1" depth="1"></a-box>
 			<a-plane static-body id="plane" color="#cac9f5" height="100" width="100" rotation="-90 0 0" position="0 -8 0"></a-plane>
 
 		</a-scene>
@@ -44,9 +46,8 @@ export default {
 	},
 	methods: {
 		test() {
-			var playerEl = document.querySelector('#snake').components['static-body'];
-			var box = document.querySelector('#box1');
-			playerEl.el.addEventListener('collide', function(e) {
+			var playerEl = document.querySelector('#camera');
+			playerEl.addEventListener('collide', function(e) {
 				console.log('Player has collided with body #' + e.detail.body.id);
 
 				e.detail.target.el;  // Original entity (playerEl).
@@ -54,7 +55,18 @@ export default {
 				e.detail.contact;    // Stats about the collision (CANNON.ContactEquation).
 				e.detail.contact.ni; // Normal (direction) of the collision (CANNON.Vec3).
 			});
-			console.log(playerEl)
+
+			// var playerEl = document.querySelector('#snake').components['static-body'];
+			// var box = document.querySelector('#box1').components['dynamic-body']
+			// playerEl.el.addEventListener('collide', function(e) {
+			// 	console.log('Player has collided with body #' + e.detail.body.id);
+
+			// 	e.detail.target.el;  // Original entity (playerEl).
+			// 	e.detail.body.el;    // Other entity, which playerEl touched.
+			// 	e.detail.contact;    // Stats about the collision (CANNON.ContactEquation).
+			// 	e.detail.contact.ni; // Normal (direction) of the collision (CANNON.Vec3).
+			// });
+			// console.log(playerEl, box)
 		}
 	},
 }
