278ff759341046c8acb3c64c80bc5801e07f8a09
yuicer <yuicer@yeah.net>
2017-12-15 18:23:20 +0800

    5=>6

--- server/chat/index.js
+++ server/chat/index.js
@@ -3,23 +3,14 @@ const room = {
   name: '聊天室'
 }
 var usernumber = 0
+var userIsAdd = false
 
 
 module.exports = function (server) {
   const io = require('socket.io')(server)
 
   io.on('connection', socket => {
-
-    socket.on('disconnect', () => {
-      var msg = {
-          type: 'info',
-          msg: socket.userName + ' 离开了' + room.name
-        }
-        --usernumber
-      io.emit('usernumber', usernumber)
-      socket.broadcast.emit('chat', msg)
-    })
-
+    // 聊天信息
     socket.on('chat', msg => {
       var msg = {
         type: 'chat',
@@ -31,16 +22,32 @@ module.exports = function (server) {
       io.emit('chat', msg)
     })
 
+    // 新增用户
     socket.on('add user', user => {
+      if (userIsAdd)
+        return ++usernumber
+      userIsAdd = true
       socket.userName = user
       socket.userColor = color.getcolor()
       var msg = {
-          type: 'info',
-          msg: user + ' 进入了' + room.name
-        }
-        ++usernumber
+        type: 'info',
+        msg: user + ' 进入了' + room.name
+      }
       io.emit('usernumber', usernumber)
       socket.broadcast.emit('chat', msg)
+    })
+
+    // 断开连接
+    socket.on('disconnect', () => {
+      if (userIsAdd) {
+        --usernumber
+        var msg = {
+          type: 'info',
+          msg: socket.userName + ' 离开了' + room.name
+        }
+        io.emit('usernumber', usernumber)
+        socket.broadcast.emit('chat', msg)
+      }
     })
 
   })
